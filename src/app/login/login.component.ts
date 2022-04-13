1d25a64359bcd2cd4bf7c97be489ea80bad045f2
Stanislau Ukolau <stanislau.ukolau@greensky.com>
2022-04-13 17:44:27 +0300

    removed material lib and added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor() { }
+
+  public loginForm: FormGroup = new FormGroup({
+    login: new FormControl('', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  public onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
+}

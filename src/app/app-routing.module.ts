839e77a93c9a69769577c6bd90ab80170e01149e
Stanislau Ukolau <stanislau.ukolau@greensky.com>
2022-04-21 01:29:22 +0300

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
-  }
+    loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'new-flat',
+    loadChildren: () => import('./pages/flat-creation/flat-creation.module').then((m) => m.FlatCreationModule),
+  },
+  {
+    path: 'flat',
+    loadChildren: () => import('./pages/flat/flat.module').then((m) => m.FlatModule),
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/user-account/user-account.module').then((m) => m.UserAccountModule),
+  },
+  {
+    path: 'advanced-search',
+    loadChildren: () => import('./pages/advanced-search/advanced-search.module').then((m) => m.AdvancedSearchModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then((m) => m.MainModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/advanced-search/advanced-search-routing.module.ts
+++ src/app/pages/advanced-search/advanced-search-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login.component';
+import { AdvancedSearchComponent } from './advanced-search.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent,
+    component: AdvancedSearchComponent,
   },
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class LoginRoutingModule {}
+export class AdvancedSearchRoutingModule {}
